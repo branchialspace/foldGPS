86ef755bed6355358ed8bbba6b786a5ede6e9410
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-12-19 14:07:21 -0500

    adjust path

--- graphgps/loader/dataset/alphafold.py
+++ graphgps/loader/dataset/alphafold.py
@@ -8,7 +8,7 @@ import re
 class Alphafold(Dataset):
     def __init__(self):
         super().__init__()
-        self.root = '/content/drive/MyDrive/protein-DATA/41k_sample_processed_ESM2'
+        self.root = '/content/41k_final_esm2'
 
     @property
     def processed_file_names(self) -> List[str]:
